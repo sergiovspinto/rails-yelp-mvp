aa61f61affeaaf3760b979cd728b20a0a2a434cf
Sergio Pinto <sergio.pinto.arq@gmail.com>
2019-08-14 15:52:12 +0200

    incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: "yemma", address: "paris", category: "japanese" )
+restaurant2 = Restaurant.create!(name: "trattoria", address: "toulouse", category: "chinese" )
+restaurant3 = Restaurant.create!(name: "mcdo", address: "bordeaux", category: "french" )
+restaurant4 = Restaurant.create!(name: "pizza hut", address: "lyon", category: "belgian" )
+restaurant5 = Restaurant.create!(name: "chez mama", address: "lyon", category: "italian" )
