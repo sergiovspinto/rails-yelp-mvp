674f7fc7824b9c967c693eac8633b629be3a5b42
Sergio Pinto <sergio.pinto.arq@gmail.com>
2019-08-14 16:28:46 +0200

    working page

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,20 @@
 ActiveRecord::Schema.define(version: 2019_08_14_103532) do
 
   create_table "restaurants", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "phone_number"
-    t.string   "category"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string   "content"
-    t.integer  "rating"
-    t.integer  "restaurant_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string "content"
+    t.integer "rating"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
